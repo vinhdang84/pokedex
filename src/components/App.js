fbc891375e354f7a56969cc370db60f9ea4716f2
Vinh Dang <vinhdang@Vinhs-MacBook-Pro.local>
2019-05-07 15:19:13 -0700

    App.js shows 151 pokemon and its images

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
-
 class App extends Component {
+  state = {
+    loading: true,
+    pokemon: [],
+  };
+
+  async componentDidMount() {
+
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => response.json())
+      .then(parsedJson => parsedJson.results.map(pokemon => (
+        {
+          name: `${pokemon.name}`.charAt(0).toUpperCase() + `${pokemon.name}`.slice(1),
+          url: `${pokemon.url}`.split('/').slice(6, -1)
+        }
+      )))
+      .then(pokemonList => this.setState({
+        pokemonList,
+        loading: false
+      }))
+      .then()
+      .catch(error => console.log('parsing failed', error))
+  }
+
   render() {
+    const { loading, pokemonList } = this.state;
     return (
-      <div className="App">
-        hellohello
+      <div>
+        {
+          !loading && pokemonList.length > 0 ? pokemonList.map(pokemon => {
+            const { name, url } = pokemon;
+            const SPRITE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${url}.png`;
+
+            return <div key={name}>
+              {url}
+              <p>
+                {name}
+                <br />
+                <img alt={name} src={SPRITE_URL} />
+              </p>
+
+            </div>
+          }) : null
+        }
       </div>
     );
   }
