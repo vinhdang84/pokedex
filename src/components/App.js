7d1d06a92011ba9e111ed818d5c5230ce439c79d
Vinh Dang <vinhdang@Vinhs-MacBook-Pro.local>
2019-05-08 12:47:11 -0700

    Changed the fetch to fetch each individual pokemon sprite. Refactored adding functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
-class App extends Component {
+const capitalize = ([first, ...rest]) =>
+  `${first.toUpperCase()}${rest.join('')}`;
+
+const getJson = url => fetch(url).then(r => r.json());
+
+const shortPokeDescription = ({ name, id, sprites: { front_default } }) => ({
+  name: capitalize(name),
+  id,
+  url: front_default,
+})
+
+class App extends React.Component {
   state = {
     loading: true,
-    pokemon: [],
+    pokemonList: []
   };
 
   async componentDidMount() {
-
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => response.json())
-      .then(parsedJson => parsedJson.results.map(pokemon => (
-        {
-          name: `${pokemon.name}`.charAt(0).toUpperCase() + `${pokemon.name}`.slice(1),
-          url: `${pokemon.url}`.split('/').slice(6, -1)
-        }
-      )))
-      .then(pokemonList => this.setState({
-        pokemonList,
-        loading: false
-      }))
-      .then()
-      .catch(error => console.log('parsing failed', error))
+    getJson('https://pokeapi.co/api/v2/pokemon/?limit=151')
+      .then(({ results: pokes }) => Promise.all(pokes.map(({ url }) => getJson(url))))
+      .then(pokes => pokes.map(shortPokeDescription))
+      .then(pokemonList =>
+        this.setState({
+          pokemonList,
+          loading: false
+        })
+      )
+      .catch(error => console.log('parsing failed', error));
   }
 
   render() {
     const { loading, pokemonList } = this.state;
     return (
       <div>
-        {
-          !loading && pokemonList.length > 0 ? pokemonList.map(pokemon => {
-            const { name, url } = pokemon;
-            const SPRITE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${url}.png`;
-
-            return <div key={name}>
-              {url}
+        {!loading && pokemonList.length > 0
+          ? pokemonList.map(({ name, url, id }) => (
+            <div key={name}>
+              {id}
               <p>
                 {name}
                 <br />
-                <img alt={name} src={SPRITE_URL} />
+                <img alt={name} src={url} />
               </p>
-
             </div>
-          }) : null
-        }
+          ))
+          : null}
       </div>
     );
   }
 }
-
 
 export default App;
